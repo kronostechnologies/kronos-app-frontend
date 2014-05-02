61cfcd0f54f549664c857a05e8cc92929704f654
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2014-05-02 14:10:52 -0400

    Added all javascript dependancies

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,58 +4,19 @@
 module.exports = function(grunt) {
   grunt.initConfig({
 
-
-    /*
-       A simple ordered concatenation strategy.
-       This will start at frontend/app/app.js and begin
-       adding dependencies in the correct order
-       writing their string contents into
-       'frontend/public/application.js'
-
-       Additionally it will wrap them in evals
-       with @ sourceURL statements so errors, log
-       statements and debugging will reference
-       the source files by line number.
-
-       You would set this option to false for
-       production.
-    */
-    neuter: {
-      'dist/application.js': 'src/*.js'
+    copy: {
+      main: { 
+	files: [
+          {expand:true, cwd: 'src/', src: '*.js', dest: 'dist/'}
+	]
+      }
     },
 
     uglify: {
-      'dist/application.min.js': 'dist/application.js'
+      'dist/application.min.js': 'dist/application.js',
+      'dist/list.min.js': 'dist/list.js'
     },
 
-    /*
-      Watch files for changes.
-
-      Changes in frontend/dependencies/ember.js or application javascript
-      will trigger the neuter task.
-
-      Changes to any templates will trigger the ember_templates
-      task (which writes a new compiled file into frontend/dependencies/)
-      and then neuter all the files again.
-    */
-    watch: {
-	    
-    },
-
-
-
-    sass: {
-      dist: {
-        options: {
-          style: 'compressed'
-        },
-        files: {
-//          'frontend/public/css/styles.css': 'frontend/app/assets/css/styles.scss',
-//          'frontend/public/css/print.css': 'frontend/app/assets/css/print.scss',
-//         'frontend/public/css/print_ie.css': 'frontend/app/assets/css/print_ie.scss'
-        }
-      }
-    },
 
     bump: {
       options: {
@@ -75,8 +36,7 @@ module.exports = function(grunt) {
     watching for changes.
   */
   grunt.registerTask('default', [
-    'neuter',
-    'sass',
+    'copy',
     'uglify'
   ]);
   

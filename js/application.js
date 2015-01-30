76b45d535d659e70b5ac2b451aab36ece6ccdac6
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2015-01-30 10:55:46 -0500

    feat: Object.create shimm

--- js/application.js
+++ js/application.js
@@ -3637,6 +3637,24 @@ Function.prototype.extend = function(object) {
 	}
 };
 
+if (typeof Object.create != 'function') {
+  Object.create = (function() {
+    var Temp = function() {};
+    return function (prototype) {
+      if (arguments.length > 1) {
+        throw Error('Second argument not supported');
+      }
+      if (typeof prototype != 'object') {
+        throw TypeError('Argument must be an object');
+      }
+      Temp.prototype = prototype;
+      var result = new Temp();
+      Temp.prototype = null;
+      return result;
+    };
+  })();
+}
+
 if (!Array.prototype.reduce)
 {
 	Array.prototype.reduce = function(fun) {

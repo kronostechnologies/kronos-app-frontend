788c609804cc55d1e867a30007d22891a19a2dec
Vincent Lemieux <vlemieux@kronostechnologies.com>
2015-03-17 10:56:58 -0400

    Wrapper div sur les showSaveDialog et showErrorDialog.

--- js/application.js
+++ js/application.js
@@ -2148,18 +2148,17 @@ var app = {
 	},
 
 	showError : function(message) {
-		$.app.showModalDialog('<h2>'+$.app._('ERROR')+'</h2>\
+		$.app.showModalDialog('<div class="error-dialog"><h2>'+$.app._('ERROR')+'</h2>\
 						<p>\
 							<strong>'+(message ? $.app._(message) : $.app._('FATAL_ERROR_OCCURED'))+'</strong>\
 							<br />\
 							'+$.app._('ERROR_TRY_AGAIN')+'\
 							<br />\
-							<br />\
 							'+$.app._('CONTACT_SUPPORT_PERSIST')+'\
 						</p>\
 						<p class="submit">\
 							<input type="submit" id="hook_create_error_close" value="'+$.app._('OK')+'" />\
-						</p>', 'fast', function() {
+						</p></div>', 'fast', function() {
 			$('#hook_create_error_close').safeClick(function() {
 				$.app.hideModalDialog('fast');
 			});
@@ -4283,7 +4282,7 @@ EditView.prototype = {
 	showSaveDialog : function() {
 		var t = this;
 
-		$.app.showModalDialog('<h2>'+$.app._('SAVE_CHANGES_TITLE')+'</h2><p>'+$.app._('SAVE_CHANGES_MESSAGE')+'</p><p class="submit"><input type="submit" id="hook_do_save_changes" value="'+$.app._('YES')+'" /> <input type="submit" id="hook_do_not_save_changes" value="'+$.app._('NO')+'" /> <a href="javascript:void(0);" id="hook_cancel_save_changes">'+$.app._('CANCEL')+'</a></p>', 'normal', function() {
+		$.app.showModalDialog('<div class="save-dialog"><h2>'+$.app._('SAVE_CHANGES_TITLE')+'</h2><p>'+$.app._('SAVE_CHANGES_MESSAGE')+'</p><p class="submit"><input type="submit" id="hook_do_save_changes" value="'+$.app._('YES')+'" /> <input type="submit" id="hook_do_not_save_changes" value="'+$.app._('NO')+'" /> <a href="javascript:void(0);" id="hook_cancel_save_changes">'+$.app._('CANCEL')+'</a></p></div>', 'normal', function() {
 			$('#hook_do_save_changes').safeClick(function() {
 				t.hideSaveDialog('save');
 			});

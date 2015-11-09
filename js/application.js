8b38b772186d641a9aa724482f4780b9b1c939c1
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2015-11-09 09:32:08 -0500

    fix: Expiration de session Siteminder

--- js/application.js
+++ js/application.js
@@ -2769,7 +2769,7 @@ var app = {
 			return true;
 		}
 
-		if(xhr.status == 401) {
+		if(xhr.status == 401 || ($.cookie('SMSESSION') === 'LOGGEDOFF')) {
 			$.app.showSessionExpiredError(xhr.responseJSON.view);
 			return false;
 		}

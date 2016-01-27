c165869e9cd0e11b7ece15b8fce16dfb89609c19
Guillaume Rochat <grochat@kronostechnologies.com>
2016-01-27 11:05:34 -0500

    Utilise un if plutot qu'un guard pour création de l'event emitter + build des min d'app.

--- js/application.js
+++ js/application.js
@@ -58,19 +58,27 @@ var app = {
 			this._JQ = jQuery(this);
 		},
 		emit: function(evt, data) {
-			!this._JQ && this._JQInit();
+			if(!this._JQ) {
+				this._JQInit();
+			}
 			this._JQ.trigger(evt, data);
 		},
 		once: function(evt, handler) {
-			!this._JQ && this._JQInit();
+			if(!this._JQ) {
+				this._JQInit();
+			}
 			this._JQ.one(evt, handler);
 		},
 		on: function(evt, handler) {
-			!this._JQ && this._JQInit();
+			if(!this._JQ) {
+				this._JQInit();
+			}
 			this._JQ.bind(evt, handler);
 		},
 		off: function(evt, handler) {
-			!this._JQ && this._JQInit();
+			if(!this._JQ) {
+				this._JQInit();
+			}
 			this._JQ.unbind(evt, handler);
 		}
 	},
